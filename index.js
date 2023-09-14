a9505774f06020de6d5b8795d3407d1092d2c07f
Alejandro <itznecro.4376761@gmail.com>
2023-09-14 01:12:55 -0300

    nuevo index

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const { DBTest } = require("./database.js");
 const foroModel = require('./foroModel.js');
+const helmet = require('helmet');
 
 
 
@@ -12,6 +13,7 @@ const PUERTO = process.env.PUERTO;
 
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + '/public'));
+app.use(helmet());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
